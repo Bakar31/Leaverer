27e49256ae0f80813fa1c1c55e6b2601c71e9ac3
abubakar-sazim <abubakar.siddik@sazim.io>
2024-01-14 01:54:35 +0600

    navbar content change

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Sazim Community App</a>
       </div>
       <div className="navbar-end">
         <button className="btn btn-ghost btn-circle">
